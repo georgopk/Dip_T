1c5d1809f8c91280919b1e3e387eb4fb6b80e5b6
Me <georgopk@ece.auth.gr>
2018-11-30 16:03:04 +0200

    Fix problem of grid discretization.
    Round the coordinates of the polygon vertices.
    As a result, vertices never have an extreamely small difference
    which leads to an impractical discretization.

--- Antenna_Simulation/regular_polygon_init.m
+++ Antenna_Simulation/regular_polygon_init.m
@@ -1,14 +1,17 @@
-%%initialize poloygon
-
+%initialize poloygon
 poly.cen =[0,0,0]; % define Polygon Center 
 poly.N = 16;  % define the shape - number of vertices (6-gon, 8-gon, N-gon)
 poly.ang_centr = 2*pi / poly.N; % Central Angle of the polygon (radians)
 poly.ang_inter = pi-(2*pi/poly.N); % Interior Angle of the polygon
 poly.side_length = 30;  % define the side length in mm (actually in the chosen unit - see "unit" variable)
 poly.r = poly.side_length/(2*sin(pi/poly.N)); % radius of the polygon (see inscribed polygons)
-poly.rotation = 0.7; % (radians) if this parameter is 0, the first Vertex of the polygon will be on the Y'Y axis
+poly.rotation = 0; %rad
 for i=1:poly.N
     temp = poly.rotation + (i-1) * poly.ang_centr;
     poly.vert(:,i) = [poly.cen(1), poly.cen(2)] + poly.r * [cos(temp), sin(temp)];
 end
 
+poly.vert = round(poly.vert,5);  % NECESSARY!!! 
+% A very small difference between the coordinates of two vertices (due to
+% aproximation), will cause an extream discretization of the grid.
+% If more precision is required, the second argument may be increased.
--- Antenna_Simulation/simple_Polygon_Patch_antenna.m
+++ Antenna_Simulation/simple_Polygon_Patch_antenna.m
@@ -41,6 +41,7 @@ clc
 %% Setup the Simulation
 physical_constants;
 unit = 1e-3; % all length in mm
+preci = 5; % Decimal digits. Only for the coordinates of the vertices.
 
 % patch width in x-direction
 patch.width  = 32; % resonant length
@@ -78,13 +79,16 @@ poly.ang_centr = 2*pi / poly.N; % Central Angle of the polygon (radians)
 poly.ang_inter = pi-(2*pi/poly.N); % Interior Angle of the polygon
 poly.side_length = 30;  % define the side length in mm (actually in the chosen unit - see "unit" variable)
 poly.r = poly.side_length/(2*sin(pi/poly.N)); % radius of the polygon (see inscribed polygons)
-poly.rotation = 0.7; %rad
+poly.rotation = 0; %rad
 for i=1:poly.N
     temp = poly.rotation + (i-1) * poly.ang_centr;
     poly.vert(:,i) = [poly.cen(1), poly.cen(2)] + poly.r * [cos(temp), sin(temp)];
 end
 
-
+poly.vert = round(poly.vert,preci);  % NECESSARY!!! 
+% A very small difference between the coordinates of two vertices (due to
+% aproximation), will cause an extream discretization of the grid.
+% If more precision is required, the second argument may be increased.
 
 %% Setup CSXCAD Geometry & Mesh
 CSX = InitCSX();
