c79dad35fb1b05126677acbe2eef4ce3c38c5076
Me <georgopk@ece.auth.gr>
2019-05-13 14:35:09 +0300

    Modify the parametric simulation for the "L" parameter

--- Parametric_MetaSlabSim/ParametricScript.m
+++ Parametric_MetaSlabSim/ParametricScript.m
@@ -1,11 +1,11 @@
 clear
 % declare the parameters
-dplen = 120:7:183;
+dplen = 160;
 disp (dplen);   % for convenience
-s=2:-0.50:0.25;
+L=[50,70];
 
 tic
-for j = 1:length(s)
+for j = 1:length(L)
     disp('*')
     disp('*')
     disp('*')
@@ -19,28 +19,28 @@ for j = 1:length(s)
     disp('*')
     disp('*')
     disp('*')
-    disp([ 'Simulating @s=' , num2str(s(j)) ])
+    disp([ 'Simulating @L=' , num2str(L(j)) ])
     for i = 1: length(dplen)
 
         % Simulation    
         disp(['Dipole length = ',num2str(dplen(i))]);   % for convenience
-        [tfreq, tport, ts11, ts21 ] = ParamMetaSlab('dipLength',dplen(i),'s',s(j));  % Run the simulation and store the results
+        [tfreq, tport, ts11, ts21 ] = ParamMetaSlab('dipLength',dplen(i),'L',L(j));  % Run the simulation and store the results
         outVal{i,:} = {tfreq, tport, ts11, ts21};       % aggregate all the results 
 
         %plot during the process
-        myplot(i,j,outVal,s(j))
+        myplot(i,j,outVal,L(j))
     end
 end
 toc
-
+disp(toc/3600)
 % % % OR plot after the process
 % % for i = 1: length(dplen)
-% %    myplot(i,outVal,s)
+% %    myplot(i,outVal,L)
 % % end
 
 
 % functions
-function myplot(n,m,outVal,s)
+function myplot(n,m,outVal,L)
     freq = cell2mat(outVal{n}(1));
     s11 = abs(cell2mat(outVal{n}(3)));
     
@@ -48,14 +48,14 @@ function myplot(n,m,outVal,s)
     if length(tmp1)>1
         warning('The resonance value apears in more than one position!!!')
     end
-    ind = freq-freq(tmp1) > -35e6 & freq-freq(tmp1) < 35e6;
+    ind = freq-freq(tmp1) > -350e6 & freq-freq(tmp1) < 350e6;
       
     % plot reflection coefficient S11
     figure(2*m-1)
     hold on
     plot( freq(ind)/1e6, 20*log10(abs(s11(ind))), 'k-', 'Linewidth', 2 );
     grid on
-    title(['reflection coefficient S_{11} @ s=', num2str(s)]);
+    title(['reflection coefficient S_{11} @ L=', num2str(L)]);
     xlabel( 'frequency f / MHz' );
     ylabel( 'reflection coefficient |S_{11}|' );
 
@@ -66,7 +66,7 @@ function myplot(n,m,outVal,s)
     hold on
     plot( freq(ind)/1e6, 20*log10(abs(s21(ind))), 'r', 'Linewidth', 2 );
     grid on
-    title( ['S_{21}    @ s=', num2str(s)] );
+    title( ['S_{21}    @ L=', num2str(L)] );
     xlabel( 'frequency f / MHz' );
     ylabel( '|S_{21}|' );
 end
