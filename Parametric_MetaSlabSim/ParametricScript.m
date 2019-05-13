ed525fd836af824034ea47502af5aabcb598d3ad
Me <georgopk@ece.auth.gr>
2019-05-13 13:06:06 +0300

    Change the parameter for the simulations. (from "g" to "s")

--- Parametric_MetaSlabSim/ParametricScript.m
+++ Parametric_MetaSlabSim/ParametricScript.m
@@ -2,29 +2,45 @@ clear
 % declare the parameters
 dplen = 120:7:183;
 disp (dplen);   % for convenience
-g=0.25:0.25:2;
+s=2:-0.50:0.25;
 
-for j=length(g)
+tic
+for j = 1:length(s)
+    disp('*')
+    disp('*')
+    disp('*')
+    disp('*')
+    disp('*')
+    disp('*')
+    disp('*')
+    disp('*')
+    disp('*')
+    disp('*')
+    disp('*')
+    disp('*')
+    disp('*')
+    disp([ 'Simulating @s=' , num2str(s(j)) ])
     for i = 1: length(dplen)
 
         % Simulation    
         disp(['Dipole length = ',num2str(dplen(i))]);   % for convenience
-        [tfreq, tport, ts11, ts21 ] = ParamMetaSlab('dipLength',dplen(i),'g',g(j));  % Run the simulation and store the results
+        [tfreq, tport, ts11, ts21 ] = ParamMetaSlab('dipLength',dplen(i),'s',s(j));  % Run the simulation and store the results
         outVal{i,:} = {tfreq, tport, ts11, ts21};       % aggregate all the results 
 
         %plot during the process
-        myplot(i,j,outVal,g(j))
+        myplot(i,j,outVal,s(j))
     end
 end
+toc
 
 % % % OR plot after the process
 % % for i = 1: length(dplen)
-% %    myplot(i,outVal,g)
+% %    myplot(i,outVal,s)
 % % end
 
 
 % functions
-function myplot(n,m,outVal,g)
+function myplot(n,m,outVal,s)
     freq = cell2mat(outVal{n}(1));
     s11 = abs(cell2mat(outVal{n}(3)));
     
@@ -39,7 +55,7 @@ function myplot(n,m,outVal,g)
     hold on
     plot( freq(ind)/1e6, 20*log10(abs(s11(ind))), 'k-', 'Linewidth', 2 );
     grid on
-    title(['reflection coefficient S_{11} @ g=', num2str(g)]);
+    title(['reflection coefficient S_{11} @ s=', num2str(s)]);
     xlabel( 'frequency f / MHz' );
     ylabel( 'reflection coefficient |S_{11}|' );
 
@@ -50,7 +66,7 @@ function myplot(n,m,outVal,g)
     hold on
     plot( freq(ind)/1e6, 20*log10(abs(s21(ind))), 'r', 'Linewidth', 2 );
     grid on
-    title( ['S_{21}    @ g=', num2str(g)] );
+    title( ['S_{21}    @ s=', num2str(s)] );
     xlabel( 'frequency f / MHz' );
     ylabel( '|S_{21}|' );
 end
